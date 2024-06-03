17f347bc45aac3592ad56cc89fb1ccf6f4e6708d
Michaelzy27 <umehmichael27@gmail.com>
2024-06-03 23:33:54 +0100

    major updates and bug fixes

--- gNaira.sol
+++ gNaira.sol
@@ -31,12 +31,17 @@ contract gNaira is ERC20Interface {
         _;
     }
 
+    modifier isMultiSigContract() {
+        require(msg.sender == multiSigAddress, "caller is not multiSig");
+        _;
+    }
+
     constructor() {
         symbol = "gNGN";
         name = "gNaira";
         decimals = 18;
         _totalSupply = 1_000_000_000_000_000_000_000_000; //initial supply set to 1 million tokens
-        governor = msg.sender;
+        governor = msg.sender;                            //the deployer is the governor
         balances[governor] = _totalSupply;
         emit Transfer(address(0), governor, _totalSupply);
 
@@ -51,9 +56,14 @@ contract gNaira is ERC20Interface {
     }
 
     function transfer(address recipient, uint amount) public returns (bool success) {
+        //ensures sender has sufficient balance
         require(amount <= balances[msg.sender], "insufficient balance");
+        //ensures sender is not blacklisted
         require(!isBlacklisted[msg.sender], "user is blacklisted and cannot send tokens");
+        //ensures receiver is not blacklisted
         require(!isBlacklisted[recipient], "recipient is blacklisted and cannot receive tokens");
+        //checks that receiving address is not address(0)
+        require(recipient != address(0), "Cannot transfer to zero address");
         balances[msg.sender] = balances[msg.sender] - amount;
         balances[recipient] = balances[recipient] + amount;
         emit Transfer(msg.sender, recipient, amount);
@@ -67,9 +77,12 @@ contract gNaira is ERC20Interface {
     }
 
     function transferFrom(address sender, address recipient, uint amount) public returns (bool success) {
+        //ensures sender has sufficient balance
         require(amount <= balances[sender], "insufficient sender balance");
         require(!isBlacklisted[sender], "sender is blacklisted and cannot send tokens");
         require(!isBlacklisted[recipient], "recipient is blacklisted and cannot receive tokens");
+        //checks that receiving address is not address(0)
+        require(recipient != address(0), "Cannot transfer to zero address");
         balances[sender] = balances[sender] - amount;
         allowed[sender][msg.sender] = allowed[sender][msg.sender] - amount;
         balances[recipient] = balances[recipient] + amount;
@@ -87,11 +100,12 @@ contract gNaira is ERC20Interface {
         governor = _governor;
     }
 
-    function mintTokens(uint amount) public  {
+    /*this function is called to mint new tokens and can only be called through multisig after
+    governor mint proposal has been approved */
+    function mintTokens(uint amount) external isMultiSigContract() {
+        //adds(mints) the amount to the total supply
         _totalSupply = _totalSupply + amount;
-        balances[governor] = balances[governor] + amount;
-        //return true;
-
+        balances[governor] = balances[governor] + amount;      
     }
 
     /*this is only called by current governor address to submit new mint proposal which will
@@ -107,11 +121,12 @@ contract gNaira is ERC20Interface {
         multiSigAddress = _mSigAddress;
     }
 
-    function burnTokens(uint amount) public {
-        
+    /*this function is called to burn tokens and can only be called through multisig after
+    a burn proposal has been approved */
+    function burnTokens(uint amount) external isMultiSigContract {
+        //subtratcs(burns) the amount from the total supply
         _totalSupply = _totalSupply - amount;
         balances[governor] = balances[governor] - amount;
-        //return true;
     }
 
     /*this is only called by current governor address to submit new burn proposal which will
@@ -122,18 +137,13 @@ contract gNaira is ERC20Interface {
 
     }
 
-    // function burnTokensFrom(address to, uint amount) external isGovernor() {
-    //     _totalSupply = _totalSupply - amount;
-    //     balances[to] = balances[to] - amount;
-    //     //return true;
-    // }
-
-    //only governor address can blaclist addresses
+    //only governor can call this function to blacklist an address
     function blacklistUser(address user) external isGovernor() {
         require(!isBlacklisted[user], "user is already blacklisted");
         isBlacklisted[user] = true;
     }
 
+    //only governor can call this function to remove blacklist for an address
     function removeBlacklist(address user) external isGovernor() {
         require(isBlacklisted[user], "user is not blacklisted");
         isBlacklisted[user] = false;
@@ -166,31 +176,46 @@ contract MultiSig {
     Proposal[] public proposals;
     mapping(uint => mapping(address => bool)) public approved;
 
+    //this modifier checks is caller is a multiSig admin
     modifier onlyAdmin() {
         require(isAdmin[msg.sender], "sender is not an admin");
         _;
     }
 
-    modifier txExists(uint _txID) {
-        require(_txID < proposals.length, "transaction does not exist");
+    //this modifier checks if a proposal exists
+    modifier pplExists(uint _pplID) {
+        require(_pplID < proposals.length, "proposal does not exist");
         _;
     } 
 
-    modifier notApproved(uint _txID) {
-        require(!approved[_txID][msg.sender], "transaction already approved");
+    //this modifier checks that a proposal is not approved yet
+    modifier notApproved(uint _pplID) {
+        require(!approved[_pplID][msg.sender], "proposal already approved");
         _;
     }
 
-    modifier notExecuted(uint _txID) {
-        require(!proposals[_txID].executed, "transaction already executed");
+    //this modifier checks if a mint/burn proposal has not been executed
+    modifier notExecuted(uint _pplID) {
+        require(!proposals[_pplID].executed, "proposal already executed");
         _;
     }
 
-//["0x634bC37172eCDD1Eb18Fb1C1f1E043006be5Cc60", "0x66D39d6dbA140b341e8B9e32f43042229ae66517"]
+    //this modifier ensures caller is gNairaContract
+    modifier onlygNairaContract() {
+        require(msg.sender == gNairaContractAddress);
+        _;
+    }
+
+    /* upon deployment, an array of admins must be initialized and the required amount of
+    signatures for proposals must be initailized. */
     constructor(address[] memory _admins, uint _required) {
+        //checks that deployer set one or more addresses as admin
         require(_admins.length > 0, "at least one admin required");
+        //checks that required set by admin is greater than one and less or equal to the number of admins
         require(_required > 0 && _required <= _admins.length, "invalid required");
 
+        /*this loops through the admin array inputed by deployer and updates the local 'admin' 
+        array with all address and also updates the isAdmin mapping for each admin*/
         for (uint i = 0; i < _admins.length; i++) {
             require(_admins[i] != address(0), "invalid adrress");
             require(!isAdmin[_admins[i]], "admin already exists");
@@ -199,65 +224,72 @@ contract MultiSig {
         }
 
         required = _required;
-
-        
-
+    
     }
 
     receive() external payable {
         emit Deposit(msg.sender, msg.value);
     }
 
+    //this function is called after deployment to set the gNairaContract address for interaction
     function setgNairaContractAddress(address _gNairaContract) public {
         gNairaContractAddress = _gNairaContract;
     }
 
-    function submit(bool _method, uint _value) public {
+    /*this method can only be called by the gNairaContract when the governor wants to mint or burn.
+    this method is used to submit a mint or burn proposal for approval by multiSig admins*/
+    function submit(bool _method, uint _value) external onlygNairaContract {
         Proposal memory newProposal = Proposal(_method, _value, false);
         proposals.push(newProposal);
         
-        //emit Submit(transactions.length - 1);
+        emit Submit(proposals.length - 1);
     }
 
-    function multisigApprove(uint _txID) external onlyAdmin txExists(_txID) notApproved(_txID) notExecuted(_txID) {
-        approved[_txID][msg.sender] = true;
-        emit Approve(msg.sender, _txID);
+    /*this function is called by admins to approve proposals
+    */
+    function adminTxApprove(uint _pplID) 
+    external onlyAdmin pplExists(_pplID) notApproved(_pplID) notExecuted(_pplID) {
+        approved[_pplID][msg.sender] = true;
+        emit Approve(msg.sender, _pplID);
     }
 
-    function getApprovalCount(uint _txID) public view returns (uint count) {
+    //returns the approval count per proposal
+    function getApprovalCount(uint _pplID) public view returns (uint count) {
+        //this loops through all admins in the 'admin array' and checks for approval for a proposal 
         for (uint i; i < admins.length; i++) {
-            if(approved[_txID][msg.sender]) {
+            if(approved[_pplID][msg.sender]) {
                 count += 1;
             }
         }
 
     }
 
-    function execute(uint _txID) external txExists(_txID) notExecuted(_txID) {  //isGovernor?
-        require(getApprovalCount(_txID) >= required, "not enough approvals");
-        Proposal storage proposal = proposals[_txID];
+    /*this function is called when a proposal is to be executed. the proposal must have
+    the minimum amount of required approvals to be executed*/
+    function execute(uint _pplID) external pplExists(_pplID) notExecuted(_pplID) {  //isGovernor?
+        require(getApprovalCount(_pplID) >= required, "not enough approvals");
+        Proposal storage proposal = proposals[_pplID];
         proposal.executed = true;
 
+        //check if the gNaira contract address has been set and creates an instance for it to use for interaction
+        require(gNairaContractAddress != address(0), "gNaira address has not been initialized");
         gNaira g_naira = gNaira(gNairaContractAddress);
 
-        if(proposal.method) {
+        if(proposal.method) {                   //true value = mint
             
             g_naira.mintTokens(proposal.value);
 
-        } else if(!proposal.method) {
+        } else if(!proposal.method) {           //false value = burn
             g_naira.burnTokens(proposal.value);
         }
         
-        // (bool success, ) = transaction.to.call{value: transaction.value}(
-        //     transaction.data
-        // );
-        // require(success, "transaction failed");
-        emit Execute(_txID);
+        emit Execute(_pplID);
     }
 
-    function revoke(uint _txID) external onlyAdmin txExists(_txID) notExecuted(_txID) {
-        require(approved[_txID][msg.sender], "transaction not approved");
-        approved[_txID][msg.sender] = false;
-        emit Revoke(msg.sender, _txID);
+    // this function is used to revoke approval for a proposal by an admin
+    function revoke(uint _pplID) external onlyAdmin pplExists(_pplID) notExecuted(_pplID) {
+        require(approved[_pplID][msg.sender], "transaction not approved");
+        approved[_pplID][msg.sender] = false;
+        emit Revoke(msg.sender, _pplID);
     } 
 }
